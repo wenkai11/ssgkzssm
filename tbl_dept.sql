4fb12364ed218d1088e474bb67d750f33614c2aa
wenkai11 <75106359+wenkai11@users.noreply.github.com>
2022-08-27 17:08:41 +0800

    Add files via upload

---
+++ tbl_dept.sql
@@ -0,0 +1,14 @@
+/*
+SQLyog Community v13.1.6 (64 bit)
+MySQL - 5.7.19 
+*********************************************************************
+*/
+/*!40101 SET NAMES utf8 */;
+
+create table `tbl_dept` (
+	`dept_id` int (11),
+	`dept_name` varchar (765)
+); 
+insert into `tbl_dept` (`dept_id`, `dept_name`) values('2','kai');
+insert into `tbl_dept` (`dept_id`, `dept_name`) values('3','开发部');
+insert into `tbl_dept` (`dept_id`, `dept_name`) values('4','测试部');
